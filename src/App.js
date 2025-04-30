91f37dd2e8c488e7d7ebca3c3700e5b2f7b53bd8
freddsterius <freddsteriusfreddster@gmail.com>
2025-04-30 13:45:09 +0200

    i deleted the logo and its import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, World.!</h1>
+      <p>This is a simple text that I am adding for the time being.</p>
     </div>
   );
 }
