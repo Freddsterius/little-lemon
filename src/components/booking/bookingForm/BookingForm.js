62f0c80ccc8369a64f07550e67da29592d25a178
freddsterius <freddsteriusfreddster@gmail.com>
2025-06-09 13:31:39 +0200

    update: I don't even remember what i changed

--- src/components/booking/bookingForm/BookingForm.js
+++ src/components/booking/bookingForm/BookingForm.js
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import "./bookingForm.css";
 import AvailableTimes from "../availableTimes/AvailableTimes";
 
-const BookingForm = ({
-  availableTimes,
-  selectedTime,
-  handleResTimeChange,
-  dispatch,
-}) => {
+const BookingForm = ({ availableTimes, dispatch }) => {
   /*Reservation Date State*/
   const [resDate, setResDate] = useState("");
   const handleResDateChange = (e) => {
@@ -19,7 +14,7 @@ const BookingForm = ({
   /*Number of Guests State*/
   const [numberOfGuests, setNumberOfGuests] = useState(1);
   const handleNumberOfGuestsChange = (e) => {
-    setNumberOfGuests(Number(e.target.value)); // value is a number
+    setNumberOfGuests(Number(e.target.value));
   };
   /*Occasion State*/
   const [occasion, setOccasion] = useState("Birthday");
@@ -30,55 +25,52 @@ const BookingForm = ({
     e.preventDefault();
   };
   return (
-    <>
-      <h2>Booking Form</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="res-date">Choose date</label>
-        <input
-          type="date"
-          id="res-date"
-          value={resDate}
-          onChange={handleResDateChange}
-          required
-        />
-        <h5>
-          Your selected date is:{" "}
-          <span style={{ color: "white" }}>{resDate || "None"}</span>
-        </h5>
-        <AvailableTimes availableTimes={availableTimes} dispatch={dispatch} />
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-          value={numberOfGuests}
-          onChange={handleNumberOfGuestsChange}
-          required
-        />
-        <h5>
-          You'll be having{" "}
-          <span style={{ color: "white" }}>{numberOfGuests}</span> guests.
-        </h5>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={resDate}
+        onChange={handleResDateChange}
+        required
+      />
+      <h5>
+        Your selected date is:{" "}
+        <span style={{ color: "white" }}>{resDate || "None"}</span>
+      </h5>
+      <AvailableTimes availableTimes={availableTimes} dispatch={dispatch} />
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={numberOfGuests}
+        onChange={handleNumberOfGuestsChange}
+        required
+      />
+      <h5>
+        You'll be having{" "}
+        <span style={{ color: "white" }}>{numberOfGuests}</span> guests.
+      </h5>
 
-        <label htmlFor="occasion">Occasion</label>
-        <select
-          id="occasion"
-          value={occasion}
-          onChange={handleOccasionChange}
-          required
-        >
-          <option value="Birthday">Birthday</option>
-          <option value="Engagement">Engagement</option>
-          <option value="Anniversary">Anniversary</option>
-        </select>
-        <h5>
-          Your selected occasion is:{" "}
-          <span style={{ color: "white" }}>{occasion}</span>
-        </h5>
-      </form>
-    </>
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={handleOccasionChange}
+        required
+      >
+        <option value="Birthday">Birthday</option>
+        <option value="Engagement">Engagement</option>
+        <option value="Anniversary">Anniversary</option>
+      </select>
+      <h5>
+        Your selected occasion is:{" "}
+        <span style={{ color: "white" }}>{occasion}</span>
+      </h5>
+    </form>
   );
 };
 
