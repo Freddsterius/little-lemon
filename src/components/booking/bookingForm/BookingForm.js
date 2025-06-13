4bfcc9838699d752c7064abe090147ad4be949c4
freddsterius <freddsteriusfreddster@gmail.com>
2025-06-13 12:10:05 +0200

    update: reworked on using useReducer

--- src/components/booking/bookingForm/BookingForm.js
+++ src/components/booking/bookingForm/BookingForm.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./bookingForm.css";
 
-const BookingForm = (/*{ availableTimes, dispatch }*/) => {
-  /*Reservation Date State*/
+const BookingForm = ({ availableTimes, dispatch }) => {
+  //Reservation Date State
   // Format today's date to YYYY-MM-DD
   const getTodayDate = () => {
     const today = new Date();
@@ -17,23 +17,7 @@ const BookingForm = (/*{ availableTimes, dispatch }*/) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
     return new Date(dateStr).toLocaleDateString(undefined, options);
   };
-
-  const [resDate, setResDate] = useState(getTodayDate());
-  const handleResDateChange = (e) => {
-    const date = e.target.value;
-    setResDate(date);
-    /* dispatch({ type: "UPDATE_TIMES", payload: date }); // lift the logic here*/
-  };
-
-  /* Reservation Time State */
-  const [availableTimes] = useState([
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
+  /************************************* */
 
   // Selected time state
   const [selectedTime, setSelectedTime] = useState("17:00");
@@ -42,6 +26,26 @@ const BookingForm = (/*{ availableTimes, dispatch }*/) => {
   const handleResTimeChange = (e) => {
     setSelectedTime(e.target.value);
   };
+
+  //Selected date
+  const [resDate, setResDate] = React.useState(getTodayDate());
+  const handleResDateChange = (e) => {
+    const newDate = e.target.value;
+    setResDate(newDate);
+
+    // Dispatch the new date to update times
+    dispatch({ type: "UPDATE_TIMES", payload: newDate });
+  };
+
+  /*
+  const [resDate, setResDate] = useState(getTodayDate());
+  const handleResDateChange = (e) => {
+    const date = e.target.value;
+    setResDate(date);
+    /* dispatch({ type: "UPDATE_TIMES", payload: date }); // lift the logic here
+  };
+  */
+  /*************************************************** */
 
   /*Number of Guests State*/
   const [numberOfGuests, setNumberOfGuests] = useState(1);
@@ -126,81 +130,3 @@ const BookingForm = (/*{ availableTimes, dispatch }*/) => {
 };
 
 export default BookingForm;
-
-/*import React, { useState } from "react";
-import "./bookingForm.css";
-import AvailableTimes from "../availableTimes/AvailableTimes";
-
-const BookingForm = ({ availableTimes, dispatch }) => {
-  /*Reservation Date State
-  const [resDate, setResDate] = useState("");
-  const handleResDateChange = (e) => {
-    const date = e.target.value;
-    setResDate(date);
-    dispatch({ type: "UPDATE_TIMES", payload: date }); // lift the logic here
-  };
-
-  /*Number of Guests State
-  const [numberOfGuests, setNumberOfGuests] = useState(1);
-  const handleNumberOfGuestsChange = (e) => {
-    setNumberOfGuests(Number(e.target.value));
-  };
-  /*Occasion State
-  const [occasion, setOccasion] = useState("Birthday");
-  const handleOccasionChange = (e) => {
-    setOccasion(e.target.value);
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="res-date">Choose date</label>
-      <input
-        type="date"
-        id="res-date"
-        value={resDate}
-        onChange={handleResDateChange}
-        required
-      />
-      <h5>
-        Your selected date is:{" "}
-        <span style={{ color: "white" }}>{resDate || "None"}</span>
-      </h5>
-      <AvailableTimes availableTimes={availableTimes} dispatch={dispatch} />
-      <label htmlFor="guests">Number of guests</label>
-      <input
-        type="number"
-        placeholder="1"
-        min="1"
-        max="10"
-        id="guests"
-        value={numberOfGuests}
-        onChange={handleNumberOfGuestsChange}
-        required
-      />
-      <h5>
-        You'll be having{" "}
-        <span style={{ color: "white" }}>{numberOfGuests}</span> guests.
-      </h5>
-
-      <label htmlFor="occasion">Occasion</label>
-      <select
-        id="occasion"
-        value={occasion}
-        onChange={handleOccasionChange}
-        required
-      >
-        <option value="Birthday">Birthday</option>
-        <option value="Engagement">Engagement</option>
-        <option value="Anniversary">Anniversary</option>
-      </select>
-      <h5>
-        Your selected occasion is:{" "}
-        <span style={{ color: "white" }}>{occasion}</span>
-      </h5>
-    </form>
-  );
-};
-
-export default BookingForm;*/
