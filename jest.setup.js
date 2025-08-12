97362b81e37dcc7bb593d181aea783f78b84a153
freddsterius <freddsteriusfreddster@gmail.com>
2025-08-12 13:21:11 +0200

    upd.: deleted all the 3 files i configured manually for unit testing

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// jest.setup.js
-import React from "react";
-import "@testing-library/jest-dom";
-import { TextEncoder, TextDecoder } from "util";
-
-global.React = React;
-global.TextEncoder = global.TextEncoder || TextEncoder;
-global.TextDecoder = global.TextDecoder || TextDecoder;
