f2ffeb8a71e1e899bedfc3a9e1e6de09967b2075
freddsterius <freddsteriusfreddster@gmail.com>
2025-08-12 10:56:51 +0200

    upd.: basically same commit as the last one. I am just fixing an error, since when i deleted the Counter app i forget to delete the imports of it in homepage.

--- src/components/homepage/HomePage.js
+++ src/components/homepage/HomePage.js
@@ -4,13 +4,9 @@ import Highlights from "../highlights/Highlights";
 import Testimonials from "../testimonials/Testimonials";
 import About from "../about/About";
 
-import Counter from "../main/Counter";
-
 const HomePage = () => {
   return (
     <>
-      {" "}
-      <Counter initialCount={0} />
       <Hero />
       <Highlights />
       <Testimonials />
