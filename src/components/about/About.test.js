38d9d768eebf09694e9f6be7f9be8f695b0e975e
freddsterius <freddsteriusfreddster@gmail.com>
2025-08-12 10:41:10 +0200

    upd.: deleted 2 files, backup.css and About.test.css

--- src/components/about/About.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import About from "./About";
-
-test("Renders the BookingForm heading", () => {
-  render(<About />);
-  const headingElement = screen.getByText("Little Plum");
-  expect(headingElement).toBeInTheDocument();
-});
